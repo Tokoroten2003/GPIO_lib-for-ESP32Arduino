74ca2458a55a2e8755173fc4caf0cc997fecf10b
T_Takebayashi <chikurin1014@gmail.com>
2023-07-09 14:51:22 +0900

    [modified] Lチカのサンプルプログラムを修正
    Changes to be committed:
    	modified:   examples/blink/main.cpp

--- examples/blink/main.cpp
+++ examples/blink/main.cpp
@@ -6,12 +6,11 @@ gpio::DigitalOut digital_out(14);
 uint8_t led_state = LOW;
 
 void setup() {
-    Serial.begin(9600);
     digital_out.setup();
 }
 
 void loop() {
-    led_state = (led_state == HIGH) ? LOW : HIGH;
     digital_out.write(led_state);
+    led_state = (led_state == HIGH) ? LOW : HIGH;
     delay(1000);
 }
